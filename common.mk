c99cbbc9ac46d3f0234278e6646acbce24868162
David Sn <divad.nnamtdeis@gmail.com>
2019-09-16 14:53:12 +0200

    msm8998-common: Introduce DeviceSettings
    Combine DisplayMode and KeyHandler to one category containing all device specific options
    
    [ divadsn | AOSiP ]:
    - Modify and adapt to msm8998-common
    - Revert commit a1b8c3b as we don't have DC-Dimming
    
    Change-Id: I3f1ae8adb9e86db1a505acb43c837bbeea6f984b
    Co-authored-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: David Sn <divad.nnamtdeis@gmail.com>
    Signed-off-by: itsjoeoui <idkwhoiam@itsjoeoui.com>

--- common.mk
+++ common.mk
@@ -165,6 +165,10 @@ PRODUCT_PACKAGES += \
     com.quicinc.cne \
     services-ext
 
+# Device Settings
+PRODUCT_PACKAGES += \
+    DeviceSettings
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
