dd9399695b4a18b9eb35673f70bbf11d7cefddd8
SirRGB <sirrgb@amyrom.ml>
2022-09-25 23:28:41 +0200

    msm8998-common: bringup xtended

--- common.mk
+++ common.mk
@@ -31,7 +31,7 @@ PRODUCT_COMPRESSED_APEX := false
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-xtended
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
